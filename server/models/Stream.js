f6a331e1d3e9e50cdf4e908cdcf6f7b95bdcc585
latoyadawson <78005063+latoyadawson@users.noreply.github.com>
2021-07-14 10:31:44 -0700

    fixes

--- server/models/Stream.js
+++ server/models/Stream.js
@@ -2,15 +2,7 @@ const { Schema } = require('mongoose');
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedSteams` array in User.js
 const streamSchema = new Schema({
-  image: [
-    {
-      type: String,
-    },
-  ],
-  description: {
-    type: String,
-  },
-  // saved book id from Imdb 
+  // saved stream id from Imdb 
     streamId: {
     type: String,
     required: true,
